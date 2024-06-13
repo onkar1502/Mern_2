e17bb95035039c1d5d5470ce61e5163186d6e2cf
onkar <onkarsonawane2003@gmail.com>
2024-06-13 14:47:03 +0530

    change in axios

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 axios.defaults.withCredentials = true;
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post('https://mern-2-one.vercel.app/register', {name, email, password})
+    axios.post('https://backend-lovat-one.vercel.app/register', {name, email, password})   
     .then(result => console.log(result))
     .catch(err => console.log(err))
   }
