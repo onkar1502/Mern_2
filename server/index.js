ee82c1534d88a0391ec93af9cf5959a11b24f4a6
onkar <onkarsonawane2003@gmail.com>
2024-06-13 10:16:41 +0530

    I change mongodb connection path

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(cors(
 ));
 app.use(express.json())
 
-mongoose.connect('mongodb+srv://yousaf:test123@cluster0.g4i5dey.mongodb.net/test?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://root:root123@cluster0.ajwdwlr.mongodb.net/data1?retryWrites=true&w=majority&appName=Cluster0');
 
 
 app.get("/", (req, res) => {
